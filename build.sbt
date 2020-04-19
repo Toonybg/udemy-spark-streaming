1d4dfc85f28d765b4d450edf1687791c543a014b
Antoni Markovski <10antoony@gmail.com>
2020-04-19 15:02:15 +0200

    update and remove unnecessary files

--- build.sbt
+++ build.sbt
@@ -5,3 +5,7 @@ version := "0.1"
 scalaVersion := "2.12.10"
 
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.5"
+
+libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.4.5"
+
+libraryDependencies += "com.github.fommil.netlib" % "all" % "1.1.2"
--- evenInAList.scala
+++ evenInAList.scala
@@ -1,10 +1,15 @@
-val x = List (1,3,5,7,9)
-val y = List (1,2,3)
 
-def check(x:List[Int]):Boolean = {
-  for (y<-x) if (y % 2 == 0) return true 
-  return false
+
+object evenInAList extends App {
+  val x = List (1,3,5,7,9)
+  val y = List (1,2,3)
+
+  def check(x:List[Int]):Boolean = {
+    for (y<-x) if (y % 2 == 0) return true
+    return false
+  }
+
+  println(check(x))
+  println(check(y))
+
 }
-
-println(check(x))
-println(check(y))
